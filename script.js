665c2f42adb7cbac8cd557d2e08c430b718918a2
Aurif0619 <167607936+Aurif0619@users.noreply.github.com>
2024-07-04 12:33:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const apiKey = "7973f2c15bc8741d52b0a5f0ef5bb2ee";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    try {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+
+        var data = await response.json();
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "imgs/clouds.png";
+
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "imgs/clear.png";
+
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "imgs/rain.png";
+
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "imgs/drizzle.png";
+
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "imgs/mist.png";
+
+        }
+        
+    } catch (error) {
+        console.error(error);
+        alert('Error fetching weather data: ' + error.message);
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+checkWeather('New York');
